3c432f24a960a1f8663a63a71c64e210fb43b48c
unknown <thiago.ferreira164@etec.sp.gov.br>
2024-04-16 22:19:47 -0300

    outro commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Title from './src/Components/Title';
+import Form from './src/Components/Form';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Capivara</Text>
+      <Title title="Meu Primeiro Formulário"></Title>
+      <Form></Form>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#add8e6',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, text} from "react-native";
+
+export default function Title()
+{
+    return(
+        <view>
+            <text property="value">my first app</text>
+        </view>
+    )
+}
